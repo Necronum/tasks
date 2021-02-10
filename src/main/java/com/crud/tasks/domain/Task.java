b31cf95dc8225654ad5c7f83a6f0403dcb2b368c
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-02-10 18:20:11 +0100

    18.3. first commit

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,11 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+}
