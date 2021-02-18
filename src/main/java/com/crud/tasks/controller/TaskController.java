d6d71eb655b2b7d5654793269c9ff544226170d1
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-02-18 16:32:55 +0100

    19.2. added service and new querries for TaskController

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,16 +13,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value ="getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
-    @GetMapping(value = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    @GetMapping("getTask")
+    public TaskDto getTask(@RequestParam Long id) throws NotFoundException {
+        return taskMapper.mapToTaskDto(service.getTaskById(id).orElseThrow(NotFoundException::new));
     }
 
     @DeleteMapping(value = "deleteTask")
