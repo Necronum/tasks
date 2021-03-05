42801281975613053aa6abdfd1a76c7922a9bb3e
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-03-05 16:30:55 +0100

    Added annotations for CORS

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://necronum.github.io/", maxAge = 3600)
 @RestController
 @RequestMapping("/v1/task")
 @RequiredArgsConstructor
