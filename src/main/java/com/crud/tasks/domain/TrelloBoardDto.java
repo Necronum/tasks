e9f5fd6fe456c92ca293895279fec0b20cb481ef
Marcin Krzyzaniak <69982435+Necronum@users.noreply.github.com>
2021-03-13 11:20:24 +0100

    22.4 Added logger and tests

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -2,18 +2,24 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrelloBoardDto {
+    @JsonProperty("id")
+    private String id;
+
     @JsonProperty("name")
     private String name;
 
-    @JsonProperty("id")
-    private String id;
 
     @JsonProperty("lists")
     private List<TrelloListDto> lists;
